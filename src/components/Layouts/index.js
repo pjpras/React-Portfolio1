fac0f3dd60433816fd9ccab5bc21668ab27eaae8
Prashant-Joshi-Only-1 <prashantjoshi430@gmail.com>
2023-03-18 22:25:35 +0530

    second commit

--- src/components/Layouts/index.js
+++ src/components/Layouts/index.js
@@ -1,10 +1,11 @@
 import Sidebar from '../Sidebar';
 import './index.scss';
-const Layout = () => {
-    return
-    <>
-    <Sidebar />
-    </>
-}
 
+const Layout = () => {
+    return (
+        <>
+           <Sidebar />
+        </>
+    )
+}
 export default Layout
